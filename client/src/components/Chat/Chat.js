7243d729860a07d49716f61a431437f89067297c
Akhil Kashyap <Akhil-Kashyap@users.noreply.github.com>
2020-06-28 14:08:59 +0530

    css changes

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,67 +1,72 @@
-import React, { useState, useEffect } from "react";
-import queryString from "query-string";
-import io from "socket.io-client";
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-import TextContainer from "../TextContainer/TextContainer";
-import Messages from "../Messages/Messages";
-import Infobar from "../Infobar/Infobar";
-import Input from "../Input/Input";
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import Infobar from '../Infobar/Infobar';
+import Input from '../Input/Input';
 
-import "./Chat.css";
+import './Chat.css';
 
 let socket;
 
 const Chat = ({ location }) => {
-  const [name, setName] = useState("");
-  const [room, setRoom] = useState("");
-  const [users, setUsers] = useState("");
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-  const ENDPOINT = "https://project-chat-application.herokuapp.com/";
+	const [ name, setName ] = useState('');
+	const [ room, setRoom ] = useState('');
+	const [ users, setUsers ] = useState('');
+	const [ message, setMessage ] = useState('');
+	const [ messages, setMessages ] = useState([]);
+	const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
-  useEffect(() => {
-    const { name, room } = queryString.parse(location.search);
+	useEffect(
+		() => {
+			const { name, room } = queryString.parse(location.search);
 
-    socket = io(ENDPOINT);
+			socket = io(ENDPOINT);
 
-    setRoom(room);
-    setName(name);
+			setRoom(room);
+			setName(name);
 
-    socket.emit("join", { name, room }, (error) => {
-      if (error) {
-        alert(error);
-      }
-    });
-  }, [ENDPOINT, location.search]);
+			socket.emit('join', { name, room }, (error) => {
+				if (error) {
+					alert(error);
+				}
+			});
+		},
+		[ ENDPOINT, location.search ]
+	);
 
-  useEffect(() => {
-    socket.on("message", (message) => {
-      setMessages((messages) => [...messages, message]);
-    });
+	useEffect(() => {
+		socket.on('message', (message) => {
+			setMessages((messages) => [ ...messages, message ]);
+		});
 
-    socket.on("roomData", ({ users }) => {
-      setUsers(users);
-    });
-  }, []);
+		socket.on('roomData', ({ users }) => {
+			setUsers(users);
+		});
+	}, []);
 
-  const sendMessage = (event) => {
-    event.preventDefault();
+	const sendMessage = (event) => {
+		event.preventDefault();
 
-    if (message) {
-      socket.emit("sendMessage", message, () => setMessage(""));
-    }
-  };
+		if (message) {
+			socket.emit('sendMessage', message, () => setMessage(''));
+		}
+	};
 
-  return (
-    <div className="outerContainer">
-      <div className="container">
-        <Infobar room={room} />
-        <Messages messages={messages} name={name} />
-        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-      </div>
-      <TextContainer users={users} />
-    </div>
-  );
+	console.log(users);
+
+	return (
+		<div className="outerContainer">
+			<div className="container">
+				<Infobar room={room} />
+				<Messages messages={messages} name={name} />
+				<Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+			</div>
+			<TextContainer users={users} />
+		</div>
+	);
 };
 
 export default Chat;
--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -13,22 +13,22 @@ const TextContainer = ({ users }) => (
           💬
         </span>
       </h1>
-      <h2>
+      {/* <h2>
         Created with React, Express, Node and Socket.IO{" "}
         <span role="img" aria-label="emoji">
           ❤️
         </span>
-      </h2>
-      <h2>
+      </h2> */}
+      {/* <h2>
         Try it out right now!{" "}
         <span role="img" aria-label="emoji">
           ⬅️
         </span>
-      </h2>
+      </h2> */}
     </div>
     {users ? (
       <div>
-        <h1>People currently chatting:</h1>
+        <h1>Users Online:</h1>
         <div className="activeContainer">
           <h2>
             {users.map(({ name }) => (
